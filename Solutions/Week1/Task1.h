2b6cedb8c9463dab89a1a95cdd6b0c3b772b3d65
Мартин Маринов <68864031+MarinovMartin@users.noreply.github.com>
2021-10-19 14:31:13 +0300

    Task 1 - week 1

---
+++ Solutions/Week1/Task1.h
@@ -0,0 +1,12 @@
+//
+// Created by 1nsaneYMD on 10/19/2021.
+//
+
+#include <iostream>
+
+namespace task1 {
+    int main() {
+        std::cout << "I :( FMI" << std::endl;
+        return 0;
+    }
+}
