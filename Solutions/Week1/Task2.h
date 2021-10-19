d971f55f11b5fb3e917c5a2fac53c99e3e5d8805
Мартин Маринов <68864031+MarinovMartin@users.noreply.github.com>
2021-10-19 14:31:21 +0300

    Task 2 - week 1

---
+++ Solutions/Week1/Task2.h
@@ -0,0 +1,14 @@
+//
+// Created by 1nsaneYMD on 10/19/2021.
+//
+
+#include <iostream>
+
+namespace task2 {
+    int main() {
+        double n;
+        std::cin >> n;
+        std::cout << "Your favourite number is " << n << std::endl;
+        return 0;
+    }
+}
