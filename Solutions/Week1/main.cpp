86fd55d3301748cc4bb5373fb137fccf44701536
Мартин Маринов <68864031+MarinovMartin@users.noreply.github.com>
2021-10-19 14:31:50 +0300

    main - week1 - solutions

---
+++ Solutions/Week1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Task1.h"
+#include "Task2.h"
+#include "Task3.h"
+#include "Task4.h"
+
+int main() {
+//    task1::main();
+//    task2::main();
+//    task3::main();
+    task4::main();
+    return 0;
+}
